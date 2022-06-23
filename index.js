643dd252805aadd296afc459687a132bf2eb7d67
Thiago099 <thiago.kaique01@gmail.com>
2022-06-23 11:25:38 -0300

    [feature] binding

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello World');
-}).listen(1337, '0.0.0.0');
+}).listen(process.env.PORT, '0.0.0.0');
 console.log('Server running at http://localhost:1337/');
