426d52e429c316642c6e57a987c7d3923e1f4e32
Thiago099 <thiago.kaique01@gmail.com>
2022-06-23 10:58:26 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello World');
+}).listen(1337, '0.0.0.0');
+console.log('Server running at http://localhost:1337/');
